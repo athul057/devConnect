1454546fecf4ffc417642b9b8d1fa7e63f5455b2
athul <athuli057@gmail.com>
2024-12-30 22:18:20 +0530

    api created

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { userAuth } = require("../middlewares/userAuth");
 const userRouter = express.Router();
 const connectionRequest = require("../models/connectRequest")
+const Users = require("../models/user")
 
 //GET ALL PENDING CONNECTION REQUEST TO THE USER.....
 //ref and populate.. to linke between to collections in mongoDB...
@@ -31,6 +32,11 @@ userRouter.get("/user/requests/connections", userAuth, async (req, res) => {
   const connectionRequests = await connectionRequest.find({ $or: [{ fromUserId: loginUser._id, status: "accepted" }, { toUserId: loginUser._id, status: "accepted" }] })
    .populate("fromUserId", ["firstName", "lastName"]).populate("toUserId", ["firstName", "lastName"])
   const filteredData = connectionRequests.map((connectionRequest) => {
+
+
+   // if (connectionRequest.fromUserId.toString()===loginUser._id.toString()){}
+   // OR
+
    if (connectionRequest.fromUserId.equals(loginUser._id)) {
     return connectionRequest.toUserId;
    }
@@ -47,4 +53,54 @@ userRouter.get("/user/requests/connections", userAuth, async (req, res) => {
  }
 })
 
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+
+ try {
+  const page = req.query.page || 1;
+  const limit = req.query.limit || 10;
+  const skip = (page - 1) * limit;
+  const logInUser = req.user;
+  const connectedUsers = await connectionRequest.find({
+   $or: [{ fromUserId: logInUser._id }, { toUserId: logInUser._id }]
+  }).select("fromUserId toUserId").lean()
+  const hideUserId = new Set()
+  connectedUsers.forEach((user) => {
+   hideUserId.add(user.fromUserId.toString());
+   hideUserId.add(user.toUserId.toString());
+
+  })
+
+  const feedUsers = await Users.find({
+   $and: [{ _id: { $nin: Array.from(hideUserId) } }, { _id: { $ne: logInUser._id } }]
+  }).select("firstName lastName about photoUrl")
+   .skip(skip)
+   .limit(limit)
+  //....................OR.............................
+  // const feedUsers = await Users.find({
+  //  _id: {
+  //   $nin: [...Array.from(hideUserId), logInUser._id]
+  //  }
+  // })
+  //....................................................
+  res.json({
+   message: "user fetched successfully",
+   data: feedUsers
+  })
+
+ } catch (error) {
+  res.status(400).json({
+   message: "ERROR " + error.message
+  })
+ }
+
+
+})
+
+
 module.exports = userRouter;
+
+
+
+////     /feed/?id===>Her id is query to get this req.query.id
+////     /feed:id=2====>Here id is params .To get this req.params.id
